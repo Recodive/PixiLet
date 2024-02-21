7bf868558c363b06e38da2d9dafdc21e8e3abfd5
Bas950 <me@bas950.com>
2024-02-21 20:02:38 +0100

    wip: tests

--- packages/pixilet/src/index.test.ts
+++ packages/pixilet/src/index.test.ts
@@ -57,15 +57,111 @@ describe("register()", () => {
 			expect(map.hasLayer(layer)).toBe(false);
 			expect(layer.addTo(map)).toBe(layer);
 			expect(map.hasLayer(layer)).toBe(true);
+
 			expect(spy).toHaveBeenCalledWith(layer.utils, expect.objectContaining({ type: "add" }));
+			expect(spy).toHaveBeenCalledTimes(1);
 
 			layer.destroy();
 
 			expect(map.hasLayer(layer)).toBe(false);
 		});
 
-		it.todo("sets the options", () => {
-			// TODO
+		it("creates a new PixiLetLayer (with doubleBuffering)", async () => {
+			const spy = vitest.fn(),
+				container = new PIXI.Container(),
+				layer = new L.PixiLetLayer(spy, {
+					container,
+					doubleBuffering: true,
+				});
+			expect(layer).toBeInstanceOf(L.PixiLetLayer);
+			expect(spy).not.toHaveBeenCalled();
+			expect(layer.options.padding).toBe(0.1);
+
+			const map = L.map(document.createElement("div")).setView([51.505, -0.09], 13);
+			expect(map.hasLayer(layer)).toBe(false);
+			expect(layer.addTo(map)).toBe(layer);
+			expect(map.hasLayer(layer)).toBe(true);
+
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			expect(spy).toHaveBeenCalledWith(layer.utils, expect.objectContaining({ type: "add" }));
+			expect(spy).toHaveBeenCalledTimes(1);
+
+			layer.destroy();
+
+			expect(map.hasLayer(layer)).toBe(false);
+		});
+
+		it("redraw()", async () => {
+			const spy = vitest.fn(),
+				container = new PIXI.Container(),
+				layer = new L.PixiLetLayer(spy, {
+					container,
+				}), map = L.map(document.createElement("div")).setView([51.505, -0.09], 13);
+
+			expect(map.hasLayer(layer)).toBe(false);
+			await layer.redraw();
+			expect(spy).toHaveBeenCalledTimes(0);
+
+			layer.addTo(map);
+
+			expect(spy).toHaveBeenCalledWith(layer.utils, expect.objectContaining({ type: "add" }));
+			expect(spy).toHaveBeenCalledTimes(1);
+
+			await layer.redraw({ type: "test" });
+
+			expect(spy).toHaveBeenCalledWith(layer.utils, expect.objectContaining({ type: "test" }));
+			expect(spy).toHaveBeenCalledTimes(2);
+
+			layer.destroy();
+
+			expect(map.hasLayer(layer)).toBe(false);
+		});
+
+		it("moveend", async () => {
+			const spy = vitest.fn(),
+				container = new PIXI.Container(),
+				layer = new L.PixiLetLayer(spy, {
+					container,
+				}), map = L.map(document.createElement("div")).setView([51.505, -0.09], 13);
+
+			layer.addTo(map);
+
+			expect(spy).toHaveBeenCalledWith(layer.utils, expect.objectContaining({ type: "add" }));
+			expect(spy).toHaveBeenCalledTimes(1);
+
+			map.setZoom(14);
+
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			expect(spy).toHaveBeenCalledWith(layer.utils, expect.objectContaining({ type: "moveend" }));
+			expect(spy).toHaveBeenCalledTimes(2);
+
+			layer.destroy();
+
+			expect(map.hasLayer(layer)).toBe(false);
+		});
+
+		it("moveend (with shouldRedrawOnMove)", async () => {
+			const spy = vitest.fn(),
+				container = new PIXI.Container(),
+				layer = new L.PixiLetLayer(spy, {
+					container,
+					shouldRedrawOnMove: () => true,
+				}), map = L.map(document.createElement("div")).setView([51.505, -0.09], 13);
+
+			layer.addTo(map);
+
+			expect(spy).toHaveBeenCalledWith(layer.utils, expect.objectContaining({ type: "add" }));
+			expect(spy).toHaveBeenCalledTimes(1);
+
+			map.setZoom(14);
+
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			expect(spy).toHaveBeenCalledWith(layer.utils, expect.objectContaining({ type: "moveend" }));
+			expect(spy).toHaveBeenCalledTimes(3);
+
+			layer.destroy();
+
+			expect(map.hasLayer(layer)).toBe(false);
 		});
 	});
 });
